d0afafa2bf8d072679d5747da0837f0662d65651
Mohamed Salman <salman.primary@gmail.com>
2022-11-03 23:22:23 +0530

    complete backend

--- SampleApp.Server.Infrastructure/Constants/CommonConstants.cs
+++ SampleApp.Server.Infrastructure/Constants/CommonConstants.cs
@@ -1,5 +1,5 @@
 ﻿namespace SampleApp.Server.Infrastructure.Constants;
-internal class CommonConstants
+internal static class CommonConstants
 {
     internal const int DefaultPageSize = 20;
     internal const string ImageStorageBasePathConfigName = "ImageStorageBasePath";
